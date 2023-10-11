9fd1d76b99fecd8bbe9968ba89f28621324b00ce
brussi <lai2-24@itismeucci.com>
2023-10-11 11:01:05 +0200

    server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,7 +14,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Server in avvio" );
+        try {
+            
+            System.out.println( "Server in avvio" );
+            ServerSocket server = new ServerSocket(3000);
+            Socket s = server.accept(); //aspetta che qualcuno si colleghi alla porta 3000 per fare il socket per parlare e ascoltare, una volta collegato qualcuno apre un altra porta per liberare la 3000
+            
+            System.out.println( "Un client si è collegato" );
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            String stringaRicevuta = in.readLine();
+            System.out.println( "Stringa ricevuta: "+stringaRicevuta );
+
+            String stringaMaiuscola = stringaRicevuta.toUpperCase();
+
+            out.writeBytes(stringaMaiuscola+'\n');
+
+            s.close();
+            server.close();
+
+        } catch (Exception e) {
+            System.out.println( e.getMessage() );
+            System.out.println( "Errore durante l'istanza del server" );
+        }
 
     }
 }
